276555f7becc953af30020cd6e5752595e67b605
kichoy <kichoy@697-334-Erebor.sf.k12.internal>
2015-01-21 10:45:09 -0800

    unfinished

--- SierpinskiTriangle.pde
+++ SierpinskiTriangle.pde
@@ -2,15 +2,29 @@ public void setup()
 {
 
 }
+
 public void draw()
 {
-
+	sierpinski(0, 50, 21);
 }
+
 public void mouseDragged()//optional
 {
 
 }
+
 public void sierpinski(int x, int y, int len) 
 {
+	//triangle(x, y, x+len/4, y+len/2, x+len/2, y);
 
+	if (len > 20)
+	{
+		sierpinski(x, y, len);
+		sierpinski(x+len/2, y, len);
+		sierpinski(x+len/4, y+len/2, len);
+	}
+	else 
+	{
+		triangle (x, y, x+len/2, y+len, x+len, y);
+	}
 }
---
+++ build-tmp/source/SierpinskiTriangle.java
@@ -0,0 +1,55 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
+public void setup()
+{
+
+}
+
+public void draw()
+{
+	sierpinski(0, 50, 21);
+}
+
+public void mouseDragged()//optional
+{
+
+}
+
+public void sierpinski(int x, int y, int len) 
+{
+	//triangle(x, y, x+len/4, y+len/2, x+len/2, y);
+
+	if (len > 20)
+	{
+		sierpinski(x, y, len);
+		sierpinski(x+len/2, y, len);
+		sierpinski(x+len/4, y+len/2, len);
+	}
+	else 
+	{
+		triangle (x, y, x+len/2, y+len, x+len, y);
+	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
